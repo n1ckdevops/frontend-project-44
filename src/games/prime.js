9f0fbb896a01bdbdad24b182c087b96fb4a88a77
muyyz <nickdevops6@gmail.com>
2025-03-29 05:26:05 +0300

    small bug fixes

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,19 +1,20 @@
 import { randomNum } from '../helpers/random.js';
 
-export const gameMessage = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+export const gameMessage =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
   if (number < 2) {
     return false;
   }
-  for (let i = 2; i < Math.sqrt(number); i += 1) {
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) return false;
   }
   return true;
 };
 
 export const gameSettings = () => {
-  const number = randomNum(2, 100);
+  const number = randomNum(2, 50);
   const question = `Question: ${number}`;
   const correctAnswer = isPrime(number) ? 'yes' : 'no';
   return [question, correctAnswer];
